0cd9bce90d22d3b761a52229064308d22d8f0d10
KryX <kry5555@wp.pl>
2026-02-19 23:33:14 +0100

    nie wiem czy działa logowanie ale już powinno

--- szpontandoo/routes/web.php
+++ szpontandoo/routes/web.php
@@ -15,3 +15,9 @@ Route::post('/login', [AuthController::class, 'login']);
 
 // wylogowanie
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+//nie wiem co to ale sie przyda trza ogarnąć po co to
+// przykładowy dashboard – chroniony
+// Route::get('/dashboard', function () {
+//     return 'Panel użytkownika: ' . auth()->user()->nick;
+// })->middleware('auth');
