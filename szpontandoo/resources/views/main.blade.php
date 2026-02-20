81f8b2c021bf88f211b2c3e5415bec8e117f4427
KryX <kry5555@wp.pl>
2026-02-20 20:13:55 +0100

    rejestracja działa ale bez id_profil

--- szpontandoo/resources/views/main.blade.php
+++ szpontandoo/resources/views/main.blade.php
@@ -11,12 +11,21 @@
 <body>
     <h1>szpontando</h1>
     <h2>tutaj poszponcisz sobie i jeszcze zarobisz</h2>
-    <a href="{{ route('login') }}">
+    @auth
+    <p>Witaj, {{ auth()->user()->nick }}!</p>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit">Wyloguj się</button>
+    </form>
+    @else
+    <p>Loguj sie pało a nie jestes taki incognito fapper <a href="{{ route('login') }}">Zatenteguj się</a> lub <a href="{{ route('register.show') }}">Zajeb konto</a>.</p>
+    @endauth
+    <!-- <a href="{{ route('login') }}">
         <button type="button">Sign_in</button>
     </a>
     <a href="{{ route('logoutt') }}">
         <button type="button">logout</button>
-    </a>
+    </a> -->
 </body>
 
 </html>
--- szpontandoo/resources/views/sign_up.blade.php
+++ szpontandoo/resources/views/sign_up.blade.php
@@ -9,7 +9,28 @@
 </head>
 
 <body>
-    tu wiersz
+    <h1>zrub se konto</h1>
+    @if ($errors->any())
+    <div style="color:red;">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form method="POST" action="{{ route('register.post') }}">
+        @csrf
+        <input type="text" name="nick" placeholder="Nick" value="{{ old('nick') }}" required>
+        <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
+        <input type="password" name="password" placeholder="Hasło" required>
+        <input type="password" name="password_confirmation" placeholder="Powtórz hasło" required>
+        <button type="submit">Zarejestruj się</button>
+    </form>
+    <a href="{{ route('login') }}">
+        <button type="button">Sign_in</button>
+    </a>
 </body>
 
 </html>
