df6eff48cd9a00008169a2d09d6285370ce20d91
KryX <kry5555@wp.pl>
2026-02-22 21:34:29 +0100

    nowa baza

--- sql/szpontando.sql
+++ sql/szpontando.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2026 at 07:09 PM
+-- Generation Time: Feb 22, 2026 at 09:33 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -122,6 +122,25 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `oferty`
+--
+
+CREATE TABLE `oferty` (
+  `id_oferty` int(255) NOT NULL,
+  `id_profil_owner` int(255) NOT NULL,
+  `adres` varchar(255) NOT NULL,
+  `typ` varchar(255) NOT NULL,
+  `cena` varchar(6) NOT NULL,
+  `do_kiedy_wazne` datetime NOT NULL,
+  `opis` text NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `stworzone` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `password_reset_tokens`
 --
 
@@ -150,6 +169,14 @@ CREATE TABLE `profil` (
   `sex` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `profil`
+--
+
+INSERT INTO `profil` (`id_profil`, `nick`, `imie`, `nazwisko`, `data_ur`, `miasto`, `email_kontaktowy`, `ocena`, `profilowe`, `sex`) VALUES
+(1, 'chuj', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'chujj', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -170,8 +197,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('jf9fXqaEzAsEq4JbLW8G0ocf2LRlfGJEjZgeGlEL', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoienRJMlIwR0VvVkJQbEp5Z2tCZjhDMk5YTTBiSjBoTFl3TENyeHdmayI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7czo1OiJyb3V0ZSI7czo1OiJsb2dpbiI7fX0=', 1771621931),
-('k61qDlWieFtetk0L21hsDhicaJ6B1ovPC0dXhVoI', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoieEJZaGhoY1BsRVVBRnpoUXRDV21Kc2N4RWtqM2VZMDVsYVI4OU9zSyI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7czo1OiJyb3V0ZSI7czo1OiJsb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1771783649);
+('5eyGdPbKQxxHMmDAkL00UvvUyFuXB1g69BrFrpkx', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiU2pvUEdMaUE2YUNtTENPbzFwSTJsdTF4SGZiSExYU2lEcW5JVkdpQSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7czo1OiJyb3V0ZSI7czo1OiJsb2dpbiI7fX0=', 1771788810),
+('jf9fXqaEzAsEq4JbLW8G0ocf2LRlfGJEjZgeGlEL', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoienRJMlIwR0VvVkJQbEp5Z2tCZjhDMk5YTTBiSjBoTFl3TENyeHdmayI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7czo1OiJyb3V0ZSI7czo1OiJsb2dpbiI7fX0=', 1771621931);
 
 -- --------------------------------------------------------
 
@@ -190,6 +217,14 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `aktywny` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nick`, `email`, `password`, `czy_admin`, `id_profil`, `created_at`, `updated_at`, `aktywny`) VALUES
+(1, 'chuj', 'chuj@email.com', '$2y$12$O7gCEy/M6rhZzY6QlMXurupyiZWHphm1m3KjvSpU4aGZzq7y6d3XG', 0, 1, '2026-02-22 17:12:27', '2026-02-22 17:12:27', 1),
+(2, 'chujj', 'chujj@email.com', '$2y$12$qV/I5JNo7uatZC3z8z2cEODLRhhPvkx3RJhdakbSK1y33XFtS6PTy', 0, 2, '2026-02-22 17:13:51', '2026-02-22 17:13:51', 0);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -234,6 +269,12 @@ ALTER TABLE `job_batches`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `oferty`
+--
+ALTER TABLE `oferty`
+  ADD PRIMARY KEY (`id_oferty`);
 
 --
 -- Indeksy dla tabeli `password_reset_tokens`
@@ -283,16 +324,22 @@ ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `oferty`
+--
+ALTER TABLE `oferty`
+  MODIFY `id_oferty` int(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `profil`
 --
 ALTER TABLE `profil`
-  MODIFY `id_profil` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_profil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
