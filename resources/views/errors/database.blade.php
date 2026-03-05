426cc3b34b86f67ce07f2e116bb1387666c62cb9
KryX <kry5555@wp.pl>
2026-03-05 12:30:59 +0100

    strona glowna sie wyswietla poprawnie jak nie jesteś zalogowany i pokazuje blad mysql w elegancki sposub

---
+++ resources/views/errors/database.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Błąd serwera</title>
+    @vite('resources/css/stop_z_wypalaniem_gał.css')
+</head>
+
+<body>
+
+    <h1>Problem z bazą danych</h1>
+    <p>Serwer bazy danych jest chwilowo niedostępny.</p>
+    <h2>wlacz xampa idioto</h2>
+</body>
+
+</html>
