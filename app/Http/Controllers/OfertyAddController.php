f9c1796478e2fff4b53018530e4ecb3c02e5b7dd
KryX <kry5555@wp.pl>
2026-03-06 10:34:14 +0100

    dodawanie działa

--- app/Http/Controllers/OfertyAddController.php
+++ app/Http/Controllers/OfertyAddController.php
@@ -7,16 +7,34 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
-
 class OfertyAddController extends Controller
 {
     public function show_form()
     {
-        return view('dodawanie_ofert');
+        return view('add-ofert');
     }
 
     public function add_ofert(Request $request)
     {
-        //
+        $request->validate([
+            'adres' => 'required|string|max:255',
+            'typ' => 'required|string|max:100',
+            'cena' => 'required|numeric|min:0',
+            'do_kiedy_wazne' => 'required|date|after:tomorrow',
+            'opis' => 'required|string|max:1000',
+        ]);
+
+        $idprofil = Auth::user()->id_profil;
+        DB::table('oferty')->insert([
+            'id_profil_owner' => $idprofil,
+            'adres' => $request->adres,
+            'typ' => $request->typ,
+            'cena' => $request->cena,
+            'do_kiedy_wazne' => $request->do_kiedy_wazne,
+            'opis' => $request->opis,
+            'status' => 'aktywna',
+        ]);
+
+        return redirect()->route('main');
     }
 }
