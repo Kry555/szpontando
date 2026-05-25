0db9ec7f299e61110cf1f747b7d443b4140f5115
Kryx <kry5555@wp.pl>
2026-05-25 21:32:49 +0200

    chyba wszytkie emaile cssowne

--- resources/views/change-email-new.blade.php
+++ resources/views/change-email-new.blade.php
@@ -5,11 +5,28 @@
   <meta charset="UTF-8">
   <title>Sprzontando</title>
   <link rel="icon" href="{{ Vite::asset('resources/images/sprzontandoico.ico') }}" type="image/x-icon">
-  @vite('resources/css/stop_z_wypalaniem_gał.css')
+  @vite('resources/css/change-email.css')
 </head>
 
 <body>
+  <header class="main-header">
 
+    <a href="{{ route('main') }}" class="logo-box">
+      <img src="{{ asset('images/logo.png') }}" alt="logo">
+    </a>
+
+    <div class="header-text">
+      <h2>Sprzontando</h2>
+      <p>New email</p>
+    </div>
+
+    <div class="header-buttons">
+      <a href="{{ route('main') }}">
+        <button type="button">Strona główna</button>
+      </a>
+    </div>
+
+  </header>
   <h1>Podaj nowy email</h1>
 
   <form method="POST" action="{{ route('email.change.send.new') }}">
